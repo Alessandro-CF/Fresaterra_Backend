d0ef64041b0a909e2c3e761aeb0075dbf9df228c
Alessandro-CF <achoque.231ds12@istta.edu.pe>
2025-05-27 18:44:21 -0500

    Modificar las rutas de los controladores

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,38 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\ProductController;
 
 use App\Http\Middleware\IsUserAuth;
 use App\Http\Middleware\IsAdmin;
 
-//* PUBLIC ROUTES
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+/**//* Rutas con el prefijo /api/v1 */
 
-Route::get('products', [ProductController::class, 'index']);
+Route::prefix('v1')->group(function () {
+    //* PUBLIC ROUTES
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 
-// * PRIVATE ROUTES
-// Para los usuarios autenticados
-Route::middleware([IsUserAuth::class])->group(function() {
-    Route::controller(AuthController::class)->group(function() {
-        Route::post('logout', 'logout');
-        Route::get('me', 'getUser');
+    Route::get('products', [ProductController::class, 'index']);
+
+    // * PRIVATE ROUTES
+    // Para los usuarios autenticados
+    Route::middleware([IsUserAuth::class])->group(function () {
+        Route::controller(AuthController::class)->group(function () {
+            Route::post('logout', 'logout');
+            Route::get('me', 'getUser');
+        });
     });
-});
 
-// Para los administradores
-Route::middleware([IsAdmin::class])->group(function() {
-    Route::controller(ProductController::class)->group(function() {
-        Route::post('products', 'store');
-        Route::get('/products/{id}', 'show');
-        Route::patch('/products/{id}', 'update');
-        Route::delete('/products/{id}', 'destroy');    
+    // Para los administradores
+    Route::middleware([IsAdmin::class])->group(function () {
+        Route::controller(ProductController::class)->group(function () {
+            Route::post('products', 'store');
+            Route::get('/products/{id}', 'show');
+            Route::patch('/products/{id}', 'update');
+            Route::delete('/products/{id}', 'destroy');
+        });
     });
 });
